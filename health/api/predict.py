df167890f55f739a1a0ef6549ec17ab47a3a3461
Liana Bittencourt <lianapbittencourt@gmail.com>
2024-09-09 18:01:58 -0300

    update scripts

--- health/api/predict.py
+++ health/api/predict.py
@@ -1,15 +1,26 @@
+import os
 import pickle
 import pandas as pd
-from ml_logic.preprocess import preprocess_data
+# from ml_logic.preprocess import preprocess_data
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+# from joblib import load
+import numpy as np
+import xgboost
 
-# import joblib
-# Carrega o modelo já treinado
-#MODELO = JOBLIB.LOAD(caminhodomodelo)
+## script dir
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
+## loading model
+model_name     = "best_xgb_model.pkl"
+model_path     = os.path.join(script_dir, "../data/model")
+abs_model_path = os.path.abspath(model_path)
+model_file     = os.path.join(model_path, model_name)
+model          = pickle.load(open(model_file, 'rb'))
+
+print("model loaded")
+## API
 app = FastAPI()
-app.state.model = load_model()
 
 # Allowing all middleware is optional, but good practice for dev purposes
 app.add_middleware(
@@ -20,33 +31,65 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-model_path = 'data/models/meu_modelo.pkl'
-with open(model_path, 'rb') as f:
-    model = pickle.load(f)
+# model = pickle.load(f)
 
-#PRECISA ATUALIZAR COM O QUE VEM DO FRONT CERTINHO
-def make_prediction(
-        pickup_datetime: str,  # 2014-07-06 19:18:00
-        pickup_longitude: float,    # -73.950655
-        pickup_latitude: float,     # 40.783282
-        dropoff_longitude: float,   # -73.984365
-        dropoff_latitude: float,    # 40.769802
-        passenger_count: int
-    ):      # 1
+#PRECISA ATUALIZAR COM O QUE VEM DO FRONT
+@app.post("/predict")
+def make_prediction(df: pd.DataFrame):
+
     #PRECISA FAZER O X_PRED COM O QUE VEM DO FRONT.
-    X_pred = pd.DataFrame(dict(
-        pickup_datetime=[pd.Timestamp(pickup_datetime, tz='UTC')],
-        pickup_longitude=[pickup_longitude],
-        pickup_latitude=[pickup_latitude],
-        dropoff_longitude=[dropoff_longitude],
-        dropoff_latitude=[dropoff_latitude],
-        passenger_count=[passenger_count],
-    ))
 
-    prediction = model.predict(X_pred)
-    probability = model.predict_proba(X_pred)
-    #PRECISA MONTAR O DICIONARIO DO RETORNO AQUI
-    prediction = {'result':int(prediction), 'probability': 2}
+
+    ## prediction
+    predict = model.predict(X_pred)
+    proba   = model.predict_proba(X_pred)
+
+    prediction = {'result': int(predict[0]), 'probability': proba.tolist()}
+
     return prediction
 
-#CARREGA A PIPELINE.
+
+X_pred = pd.DataFrame([{
+        'var_1': np.random.randint(1, 3),
+        'var_2': np.random.randint(1, 3),
+        'var_3': np.random.randint(1, 3),
+        'var_4': np.random.randint(1, 3),
+        'var_5': np.random.randint(1, 3),
+        'var_6': np.random.randint(1, 3),
+        'var_7': np.random.randint(1, 3),
+        'var_8': np.random.randint(1, 3),
+        'var_9': np.random.randint(1, 3),
+        'var_10': np.random.randint(1, 3),
+        'var_11': np.random.randint(1, 3),
+        'var_12': np.random.randint(1, 3),
+        'var_13': np.random.randint(1, 3),
+        'var_14': np.random.randint(1, 3),
+        'var_15': np.random.randint(1, 3),
+        'var_16': np.random.randint(1, 3),
+        'var_17': np.random.randint(1, 3),
+        'var_18': np.random.randint(1, 3),
+        'var_19': np.random.randint(1, 3),
+        'var_20': np.random.randint(1, 3),
+        'var_21': np.random.randint(1, 3),
+        'var_22': np.random.randint(1, 3),
+        'var_23': np.random.randint(1, 3),
+        'var_24': np.random.randint(1, 3),
+        'var_25': np.random.randint(1, 3),
+        'var_26': np.random.randint(1, 3),
+        'var_27': np.random.randint(1, 3),
+        'var_28': np.random.randint(1, 3),
+        'var_29': np.random.randint(1, 3),
+        'var_30': np.random.randint(1, 3),
+        'var_31': np.random.randint(1, 3),
+        'var_32': np.random.randint(1, 3),
+        'var_33': np.random.randint(1, 3),
+        'var_34': np.random.randint(1, 3),
+        'var_35': np.random.randint(1, 3),
+        'var_36': np.random.randint(1, 3),
+        'var_37': np.random.randint(1, 3),
+        'var_38': np.random.randint(1, 3),
+        'var_39': np.random.randint(1, 3),
+        'var_40': np.random.randint(1, 3)
+    }])
+
+make_prediction(X_pred)
